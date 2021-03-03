8cdcb9ade795b6028ea4d105a5e26df0248167f1
Jan Larsen <jan@javacon.dk>
2021-03-03 18:41:14 +0100

    New examples for Operators and Math

---
+++ examples/src/main/java/dk/logb/javase/c07operatorsandmath/MonteCarloPi.java
@@ -0,0 +1,39 @@
+package dk.logb.javase.c07operatorsandmath;
+
+import java.util.Random;
+
+/**
+ * Demostrates how to find Pi using the (rather stupid) Monte Carlo algorithm.
+ * 
+ * Monte Carlo algorithms are named after the city state renouned for its
+ * casinos because they rely on chance and statistics.
+ * 
+ * The idea is that a random point in a 1*1 square will be either in a quarter
+ * circle with radius 1 or not. Counting the number of hits compared to the
+ * total will give a number proportional to Pi
+ *
+ */
+
+public class MonteCarloPi {
+	static final long N = 100000000l;
+
+	public static void main(String[] args) {
+		long n = 0;
+		Random rand = new Random();
+		for (long i = 1; i < N; i++) {
+			/* a random x,y value */
+			double x = rand.nextDouble();
+			double y = rand.nextDouble();
+			/* distance from 0,0 */
+			double r = Math.sqrt(x * x + y * y);
+			/* inside or outside? */
+			if (r < 1) {
+				n++;
+			}
+			if (i % 10000 == 0) {
+				System.out.println(4.0 * n / i);
+			}
+		}
+		System.out.println("\n"+Math.PI);
+	}
+}
---
+++ examples/src/main/java/dk/logb/javase/c07operatorsandmath/TestBigDecimal.java
@@ -0,0 +1,39 @@
+package dk.logb.javase.c07operatorsandmath;
+
+import java.math.BigDecimal;
+
+/*
+ * Demonstrates a few traps related to floating point literals and BigDecimal
+ * double omly has a precision of 15 decimals.
+ * 1,111,111,111.11  dollars in indonesian rupiahs is 15,686,436,434,867,81
+ * and cannot be held in a double
+ * 
+ * Therefore BigDecimal is the standard in the financial industry.
+ */
+public class TestBigDecimal {
+	/* Note that, amazingly, it is possible to define a literal that does not fit in the variable type! */
+	static double x =  1.111000000000000000000000001d;
+	static String s = "1.111000000000000000000000001";
+	static double y =  1.111d;
+	
+	public static void main(String[] args) {
+		System.out.println("Print the value of x = 1.111000000000000000000000001d");
+		System.out.println(""+x);
+		
+		System.out.println("Print the value of s = \"1.111000000000000000000000001\";");
+		System.out.println(s);
+		
+		BigDecimal bd = new BigDecimal(x);
+		System.out.println("Print the value of a BigDecimal based on x");
+		System.out.println(bd);
+		
+		bd = new BigDecimal(y);
+		System.out.println("Print the value of a BigDecimal based on y");
+		System.out.println(bd);
+		
+		bd = new BigDecimal(s);
+		System.out.println("Print the value of a BigDecimal based on s");
+		System.out.println(bd);
+				
+	}
+}
