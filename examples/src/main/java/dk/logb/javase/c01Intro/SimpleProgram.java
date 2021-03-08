d48a12dfebba9dbc09ccc31dbbe03e0e345de9ef
Jan Larsen <jan@javacon.dk>
2021-03-08 14:45:05 +0100

    Added SimpleProgram

---
+++ examples/src/main/java/dk/logb/javase/c01Intro/SimpleProgram.java
@@ -0,0 +1,11 @@
+package dk.logb.javase.c01Intro;
+/**
+ * This class demonstrates one of the simplest Java programs possible
+ */
+public class SimpleProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java SE Foundation");
+	}
+
+}
