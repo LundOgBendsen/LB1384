151159a8c0e87f6a02b09dddeb3cbb27fb1b9afe
Jan Larsen <jan@javacon.dk>
2021-03-15 16:57:10 +0100

    Example for character collation added

---
+++ examples/src/main/java/dk/logb/javase/c10strings/ex01/CharacterCollation.java
@@ -0,0 +1,40 @@
+package dk.logb.javase.c10strings.ex01;
+
+import java.text.Collator;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Demonstrates how character collation is a necessary concept when sorting strings and how
+ * java.text.Collator and java.util.Locale can help achieve correct sorting.
+ * 
+ * This is a real world problem that has stumped experienced developers for days!
+ *
+ */
+public class CharacterCollation {
+	public static void main(String[] args) {
+		/* Create a list of strings that we will want to sort */
+		List<String> a = Arrays.asList("Årslev", "Ejby", "Ésbjerg", "Exby", "Aarhus", "Ålborg", "Asnæs");
+		
+		/* Sort the strings using the default collation and print them */
+		Collections.sort(a);
+		for (String string : a) {
+			System.out.println(string);
+		}
+		
+		System.out.println();
+		
+		/* Now create a Danish Collator and sort the strings with it */
+		Collator col = Collator.getInstance(new Locale("da", "DK"));
+		Collections.sort(a, col);
+		for (String string : a) {
+			System.out.println(string);
+		}
+		
+		/* Note that É is now considered equivalent to E and Aa to Å */
+
+	}
+
+}
