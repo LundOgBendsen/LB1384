34939f6ad51d75a2f4f8e5efa3257ad73e83c92e
Jan Larsen <jan@javacon.dk>
2021-03-17 17:25:47 +0100

    Similar exercises for arrays and ArrayList added

---
+++ solutions/src/main/java/lab11/arr1/chess/TestChessClub.java
@@ -0,0 +1,30 @@
+package lab11.arr1.chess;
+
+public class TestChessClub {
+	public static void main(String[] args) {
+		String[] initialMembers = {"Adam", "Charlotte", "David"};
+		ChessClub cc = new ChessClub("The Chessters", initialMembers);
+		System.out.println(cc);
+		cc.addMember(null);
+		System.out.println(cc);
+		cc.addBoardPosition("Adam", 1);
+		System.out.println(cc);
+		cc.addBoardPosition("Adam", 0);
+		System.out.println(cc);
+		cc.addBoardPosition("Adam", 5);
+		System.out.println(cc);
+		cc.addBoardPosition("Bobby", 2);
+		System.out.println(cc);
+		cc.addBoardPosition("Charlotte", 3);
+		System.out.println(cc);
+		cc.addBoardPosition("David", 4);
+		System.out.println(cc);
+		cc.addMember("Brenda");
+		System.out.println(cc);
+		cc.addBoardPosition("Brenda", 2);
+		System.out.println(cc);
+		cc.removeMember("Adam");
+		System.out.println(cc);
+
+	}
+}
