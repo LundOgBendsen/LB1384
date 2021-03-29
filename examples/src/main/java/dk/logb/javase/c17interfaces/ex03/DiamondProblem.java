3640ac9c828b4f276531f7143f2722f0ff436fb4
Jan Larsen <jan@javacon.dk>
2021-03-29 12:46:48 +0200

    Diamond problem example - if anyone asks

---
+++ examples/src/main/java/dk/logb/javase/c17interfaces/ex03/DiamondProblem.java
@@ -0,0 +1,25 @@
+package dk.logb.javase.c17interfaces.ex03;
+ /**
+  * Demonstrates the default key word in interfaces and how it can lead to a conflict - the so called diamond problem
+  *
+  */
+public class DiamondProblem implements I1, I2 {
+	/* The following override is necessary for the program to compile since both i1 and i2 have default implementations */
+	@Override
+	public void a() {
+		I1.super.a();
+	}
+
+}
+
+interface I1 {
+	default void a() {
+		System.out.println("i1.a()");
+	}
+}
+
+interface I2 {
+	default void a() {
+		System.out.println("i2.a()");
+	}
+}
