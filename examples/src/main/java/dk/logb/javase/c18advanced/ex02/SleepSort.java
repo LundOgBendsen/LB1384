5b3b9ef937120c9388297c4cc1b5cf49998b40a0
Jan Larsen <jan@javacon.dk>
2021-04-06 15:38:15 +0200

    Added SleepSort which demonstrates how to (in a silly way) sort integers given as input

---
+++ examples/src/main/java/dk/logb/javase/c18advanced/ex02/SleepSort.java
@@ -0,0 +1,41 @@
+package dk.logb.javase.c18advanced.ex02;
+/**
+ * Demonstrates how to (in a silly way) sort integers given as input
+ *
+ */
+public class SleepSort {
+	/**
+	 * Prints out arguments in order
+	 * @param args command line arguments that must be parsable as integers. Example: 4 1 11 9 5
+	 */
+	public static void main(String[] args) {
+		for (String arg : args) {
+			int n = Integer.parseInt(arg);
+			new Thread(new Sleeper(n)).start();
+		}
+	}
+}
+ /**
+  * This is the runnable class that contains the code to be run in each thread.
+  *
+  */
+class Sleeper implements Runnable{
+	private final int secs;
+	public Sleeper(int secs) {
+		this.secs = secs;
+	}
+
+	/**
+	 * When a thread is started with an instance Runnable, the run method will be called.
+	 */
+	@Override
+	public void run() {
+		/* Sleep for the number of seconds this object was asked to sleep */ 
+		try {
+			Thread.sleep(secs*1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace(); // Not very nice, but should not happen normally
+		}
+		System.out.println(secs); // Print out the number
+	}
+}
