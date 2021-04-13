16eae9d578fb00cd9b996d98356dae23f7ade576
Jan Larsen <jan@javacon.dk>
2021-04-13 14:07:17 +0200

    Assert messages

--- solutions/src/test/java/c18junit1/write/test/PersonTest.java
+++ solutions/src/test/java/c18junit1/write/test/PersonTest.java
@@ -13,11 +13,11 @@ class PersonTest {
 		LocalDate birthDay = LocalDate.of(1970, 5, 13);
 		LocalDate testDay = LocalDate.of(2021, 4, 13);
 		Person p =  new Person(birthDay);
-		assertEquals(50, p.getAgeAt(testDay));
+		assertEquals(50, p.getAgeAt(testDay), "Before birthday");
 		testDay = LocalDate.of(2021, 5, 13);
-		assertEquals(51, p.getAgeAt(testDay));
+		assertEquals(51, p.getAgeAt(testDay), "On birthday");
 		testDay = LocalDate.of(2021, 6, 13);
-		assertEquals(51, p.getAgeAt(testDay));
+		assertEquals(51, p.getAgeAt(testDay), "After birthday");
 	}
 
 }
