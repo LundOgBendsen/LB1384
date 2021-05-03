e3519013536a9dc6e8881f1ac47caf3b2a038788
Jan Larsen <jan@javacon.dk>
2021-05-03 10:04:39 +0200

    Formatting and translation of overlooked examples

--- examples/src/main/java/dk/logb/javase/c12exceptions/ex03/AccessIllegalIndexV1.java
+++ examples/src/main/java/dk/logb/javase/c12exceptions/ex03/AccessIllegalIndexV1.java
@@ -1,21 +1,18 @@
 package dk.logb.javase.c12exceptions.ex03;
 
 /*
- * Viser en RuntimeException - nemlig IndexOutOfBoundsException,
- * der ikke h�ndteres
+ * Demonstrates a RuntimeException - namely IndexOutOfBoundsException,
+ * which is not handled
  */
 
-public class AccessIllegalIndexV1
-{
-  public static void main(final String[] args)
-  {
-    String[] s = new String[3];
-    s[0] = "Hello";
-    s[1] = " my";
-    s[2] = " friend!\n";
-    for (int n = 0; n <= 3; n++)
-    {
-      System.out.print(s[n]);
-    }
-  }
+public class AccessIllegalIndexV1 {
+	public static void main(final String[] args) {
+		String[] s = new String[3];
+		s[0] = "Hello";
+		s[1] = " my";
+		s[2] = " friend!\n";
+		for (int n = 0; n <= 3; n++) {
+			System.out.print(s[n]);
+		}
+	}
 }
--- examples/src/main/java/dk/logb/javase/c12exceptions/ex03/AccessIllegalIndexV2.java
+++ examples/src/main/java/dk/logb/javase/c12exceptions/ex03/AccessIllegalIndexV2.java
@@ -1,33 +1,27 @@
 package dk.logb.javase.c12exceptions.ex03;
 
 /*
- * Viser en RuntimeException - nemlig IndexOutOfBoundsException,
- * der h�ndteres. Viser ogs� at det her virker t�beligt...
- * Faktisk er det et eksempel p� rigtig d�rlig brug af
- * Exceptions - nemlig til at opn� almindeligt flow med -
- * dvs. vi helt bevidst forventer at f� en fejl hver gang....!
+ * Demonstrates a RuntimeException - namely IndexOutOfBoundsException,
+ * which is handled. Also demostrates that in this case it is malplaced
+ * Since we expect an error in each program execution, this is an example
+ * of the anti-pattern of flow-control-by-exception.
  */
 
-public class AccessIllegalIndexV2
-{
-  public static void main(final String[] args)
-  {
-    String[] s = new String[3];
-    s[0] = "Hej";
-    s[1] = " med";
-    s[2] = " dig!\n";
-    try
-    {
-      int n = 0;
-      while (true)
-      {
-        System.out.print(s[n]);
-        n++;
-      }
-    }
-    catch (IndexOutOfBoundsException e)
-    {
-      System.out.println("Vi forhindrede programmet i at g� ned, " + "selvom vi tilgik et ulovligt index i arrayet");
-    }
-  }
+public class AccessIllegalIndexV2 {
+	public static void main(final String[] args) {
+		String[] s = new String[3];
+		s[0] = "Hello";
+		s[1] = " my";
+		s[2] = " friend!\n";
+		try {
+			int n = 0;
+			while (true) {
+				System.out.print(s[n]);
+				n++;
+			}
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println(
+					"Prevented the program from crashing even though it threw an exception");
+		}
+	}
 }
