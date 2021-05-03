7f9b274354c5178e55a6286774555262a845f361
Jan Larsen <jan@javacon.dk>
2021-05-03 10:05:50 +0200

    Include example of variable hiding

--- examples/src/main/java/dk/logb/javase/c11inheritanceandcomposition/ex05/MethodsAreInherited.java
+++ examples/src/main/java/dk/logb/javase/c11inheritanceandcomposition/ex05/MethodsAreInherited.java
@@ -1,7 +1,7 @@
 package dk.logb.javase.c11inheritanceandcomposition.ex05;
 
 /**
- * Demonstrates that subclasses inherit methods
+ * Demonstrates that subclasses inherit methods and can hide fields
  */
 public class MethodsAreInherited {
 	public static void main(final String[] args) {
@@ -9,13 +9,17 @@ public class MethodsAreInherited {
 		Car car = new Car();
 		car.maxSpeed = 200;
 		car.numberOfWheels = 4;
-
+		car.speed = 100;
+		
 		System.out.println(car.isUnderMaxSpeed(140));
+		/* Might produce unexpected result. Why? */
+		System.out.println(car.getSpeed());
 	}
 }
 
 class Vehicle {
 	int maxSpeed;
+	int speed;
 
 	boolean isUnderMaxSpeed(final int speed) {
 		if (speed < maxSpeed) {
@@ -24,8 +28,14 @@ class Vehicle {
 			return false;
 		}
 	}
+	
+	int getSpeed() {
+		return speed;
+	}
 }
 
 class Car extends Vehicle {
 	int numberOfWheels;
+	/* Hides the speed variable in Vehicle */
+	int speed;
 }
